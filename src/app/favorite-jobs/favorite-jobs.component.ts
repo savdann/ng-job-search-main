0fbf65b4d596527668f8e1c015c57dbb4ecfb0eb
daniela.savastre <daniela.savastre@adesso.de>
2024-07-16 08:52:59 +0200

    fix refresh problem in favorite-jobs component
    fix refresh on favorites

--- src/app/favorite-jobs/favorite-jobs.component.ts
+++ src/app/favorite-jobs/favorite-jobs.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { JobService } from '../services/job.service';
+import {Job} from "../model/job.model";
 
 @Component({
   selector: 'app-favorite-jobs',
@@ -8,13 +9,13 @@ import { JobService } from '../services/job.service';
   templateUrl: './favorite-jobs.component.html',
   styleUrl: './favorite-jobs.component.css'
 })
-export class FavoriteJobsComponent implements OnInit {
+export class FavoriteJobsComponent {
+  favorites: Job[] = [];
 
   constructor(private jobService: JobService) {
+    this.jobService.loadAllJobs().subscribe(() => {
+      this.favorites = this.jobService.favoriteJobs();
+    });
   }
-  ngOnInit() {
-
-  }
-  favorites = this.jobService.favoriteJobs();
 
 }
